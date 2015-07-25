b453c78a1b0b42c7c60184fa18157188e9197990
Alexander Vasileyko <alexnodejs@yahoo.com>
2015-07-25 15:44:10 +0300

    Implement basic Auth

---
+++ api/policies/isAuthenticated.js
@@ -0,0 +1,9 @@
+module.exports = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    return res.forbidden({
+    	message: 'You are not authorized, please sign-in first.'
+    });
+  }
+};
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,19 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+
+  'get /login': {
+    view: 'login'
+  },
+
+  'get /signup': {
+    view: 'signup'
+  },
+
+  'post /login': 'AuthController.login',
+
+  '/logout': 'AuthController.logout'
 
   /***************************************************************************
   *                                                                          *
